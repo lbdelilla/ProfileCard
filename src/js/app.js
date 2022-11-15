3914e9fd20f62f28f8544d59dd85628df0ddbb76
lbdelilla <lbdelilla@gmail.com>
2022-11-15 19:28:41 +0000

    Done and done

--- src/js/app.js
+++ src/js/app.js
@@ -36,7 +36,7 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname} </h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city},${variables.country}</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/elonmusk"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/lbdelilla"><i class="fab fa-github"></i></a></li>
             <li><a href="https://www.linkedin.com/in/luc%C3%ADabelen/"><i class="fab fa-linkedin"></i></a></li>
@@ -54,21 +54,23 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://img.freepik.com/vector-gratis/fondo-paisaje-primavera-hermosa-dibujada_23-2148857331.jpg?w=2000",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://static.pokemonpets.com/images/monsters-images-800-800/143-Snorlax.webp",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "lbdelilla",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Name",
+    lastname: "Lastname",
+    role: "Role",
+    country: "Country",
+    city: "City"
   };
   render(window.variables); //render the card for the first time
 
